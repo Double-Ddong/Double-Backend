46e2180f6006fb2b9b1d94b5137cafe04ca38d7b
bohyeon <980926a@gmail.com>
2022-05-03 17:48:31 +0900

    기능 : 회원탈퇴

--- routes/api/setting/index.js
+++ routes/api/setting/index.js
@@ -4,5 +4,6 @@ var router = express.Router();
 router.use("/settingMain", require("./settingMain"));
 router.use("/modifyProfile", require("./modifyProfile"));
 router.use("/setScope", require("./setScope"));
+router.use("/deleteUser", require("./deleteUser"));
 
 module.exports = router;
