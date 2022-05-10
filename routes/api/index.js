44b5a98e1e1c4ed79e6c0c39748910967dd20ac5
soyeon <no02031@naver.com>
2022-05-10 10:37:35 +0900

    수정:chat삭제

--- routes/api/index.js
+++ routes/api/index.js
@@ -4,6 +4,5 @@ var router = express.Router();
 router.use('/auth', require('./auth/index'));
 router.use('/setting', require('./setting/index'));
 router.use('/main', require('./main/index'));
-router.use('/chat', require('./chat/index'));
 
 module.exports = router;
